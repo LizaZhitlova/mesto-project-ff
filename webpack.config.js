27d05239adce0ce6604d90d856670b7c5fde831e
Liza Zhitlova <zhitlova.liza0712@yandex.ru>
2025-01-31 00:21:39 +0300

    add webpack build js -success

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 // webpack.config.js
 const path = require('path'); // подключаем path к конфигу вебпак
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключаем HTML плагин
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин, который будет каждый раз при сборке проекта удалять содержимое папки dist.
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // подключаем к проекту плагин  mini-css-extract-plugin, с помощью которого webpack берёт много CSS-файлов и объединяет их в один.
+// const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключаем HTML плагин
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин, который будет каждый раз при сборке проекта удалять содержимое папки dist.
+// // const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // подключаем к проекту плагин  mini-css-extract-plugin, с помощью которого webpack берёт много CSS-файлов и объединяет их в один.
 
 module.exports = {
     entry: { main: './src/index.js' },
@@ -29,33 +29,33 @@ module.exports = {
             use: 'babel-loader',
             // исключает папку node_modules, файлы в ней обрабатывать не нужно
             exclude: '/node_modules/'
-          },
+          }
           // добавили правило для обработки файлов разных типов
-  {
-    // регулярное выражение, которое ищет все файлы с такими расширениями
-    test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-    type: 'asset/resource'
-  },
+//   {
+//     // регулярное выражение, которое ищет все файлы с такими расширениями
+//     test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+//     type: 'asset/resource'
+//   },
 
-  {
-    // применять это правило только к CSS-файлам
-    test: /\.css$/,
-    // при обработке этих файлов нужно использовать
-    // MiniCssExtractPlugin.loader и css-loader
-    use: [MiniCssExtractPlugin.loader, {
-      loader: 'css-loader'
-    }]
-  }
+//   {
+//     // применять это правило только к CSS-файлам
+//     test: /\.css$/,
+//     // при обработке этих файлов нужно использовать
+//     // MiniCssExtractPlugin.loader и css-loader
+//     use: [MiniCssExtractPlugin.loader, {
+//       loader: 'css-loader'
+//     }]
+//   }
           ]
-      },
+      }
 
-      plugins: [ //plugins это массив, в который мы передаём объекты опций 
-        new HtmlWebpackPlugin({
-          template: './src/index.html' // путь к файлу index.html -объект опций 
-        }),
-        new CleanWebpackPlugin(), // вызываем опцию CleanWebpackPlugin 
-        new MiniCssExtractPlugin(),// подклюсаем опцию для объединения CSS файлов,
-      ]
+    //   plugins: [ //plugins это массив, в который мы передаём объекты опций 
+    //     new HtmlWebpackPlugin({
+    //       template: './src/index.html' // путь к файлу index.html -объект опций 
+    //     }),
+    //     new CleanWebpackPlugin(), // вызываем опцию CleanWebpackPlugin 
+    //     // new MiniCssExtractPlugin(),// подклюсаем опцию для объединения CSS файлов,
+    //   ]
     
 }
 
